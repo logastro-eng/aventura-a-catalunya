5046e076fe160a94c800d8bd3119eee2a6002e7f
logastro-eng <logastro@hotmail.com>
2026-01-02 14:28:56 +0100

    Add files via upload

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,11 @@ export default defineConfig({
   define: {
     'process.env.API_KEY': JSON.stringify(process.env.API_KEY)
   },
+  resolve: {
+    extensions: ['.mjs', '.js', '.ts', '.jsx', '.tsx', '.json']
+  },
   build: {
     outDir: 'dist',
+    sourcemap: false
   }
 });
